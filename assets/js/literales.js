aca355b4852548244a2b41542fbc6cc24bf09987
luisaGonzales <luisagc711@gmail.com>
2017-09-21 07:23:36 -0500

    ES6

--- assets/js/literales.js
+++ assets/js/literales.js
@@ -9,11 +9,11 @@ const app = {
 
     init: function () {
         // Elementos
-        app.botonAgregar = $("#agregar");
-        app.botonMostrar = $("#mostrar");
-        app.botonTopTecnico = $("#top-tecnico");
-        app.botonTopHSE = $("#top-hse");
-        app.resultado = $("#contenedor-estudiantes");
+        app.botonAgregar = $(`#agregar`);
+        app.botonMostrar = $(`#mostrar`);
+        app.botonTopTecnico = $(`#top-tecnico`);
+        app.botonTopHSE = $(`#top-hse`);
+        app.resultado = $(`#contenedor-estudiantes`);
         app.setup();
     },
     setup: function () {
@@ -51,44 +51,64 @@ const app = {
         return app.estudiantes;
     },
     agregarEstudiante: function () {
-        let userNombre = prompt("¿Cuál es el nombre del estudiante?");
-        let userPuntosTecnicos = parseInt(prompt("Ingresa el porcentaje técnico"));
-        let userPuntosHSE = parseInt(prompt("Ingresa el porcentaje de habilidades socio-emocionales"));
-        let userEstado = 'Activo';
+        let userNombre = prompt(`¿Cuál es el nombre del estudiante?`);
+        let userPuntosTecnicos = parseInt(prompt(`Ingresa el porcentaje técnico`));
+        let userPuntosHSE = parseInt(prompt(`Ingresa el porcentaje de habilidades socio-emocionales`));
         let promedio = parseInt(userPuntosTecnicos + userPuntosHSE) / 2;
-    
-        let estudiante = {
-            nombre: userNombre.charAt(0).toUpperCase() + userNombre.slice(1),
-            puntosTecnicos: userPuntosTecnicos,
-            puntosHSE: userPuntosHSE,
-            estado: userEstado,
-            promedio: promedio
-        };
-        app.estudiantes.push(estudiante);
-        return estudiante;
+        let userEstado = 'Activo';
+
+        if (userNombre != "" && userPuntosTecnicos != "" && userPuntosHSE != "") {
+            if ((isNaN(userNombre)) && (!isNaN(userPuntosTecnicos)) && (!isNaN(userPuntosHSE))) {
+                let estudiante = {
+                    nombre: userNombre.charAt(0).toUpperCase() + userNombre.slice(1),
+                    puntosTecnicos: userPuntosTecnicos,
+                    puntosHSE: userPuntosHSE,
+                    estado: userEstado,
+                    promedio: promedio
+                };
+                app.estudiantes.push(estudiante);
+                return estudiante;
+            } else {
+                alert(`No se ha ingresado alguno de los datos correctamente`);
+                userNombre = prompt(`¿Cuál es el nombre del estudiante?`);
+                userPuntosTecnicos = parseInt(prompt(`Ingresa el porcentaje técnico`));
+                userPuntosHSE = parseInt(prompt(`Ingresa el porcentaje de habilidades socio-emocionales`));
+                promedio = parseInt(userPuntosTecnicos + userPuntosHSE) / 2;
+            }
+        } else {
+            alert(`No se ha ingresado alguno de los datos`);
+            userNombre = prompt(`¿Cuál es el nombre del estudiante?`);
+            userPuntosTecnicos = parseInt(prompt(`Ingresa el porcentaje técnico`));
+            userPuntosHSE = parseInt(prompt(`Ingresa el porcentaje de habilidades socio-emocionales`));
+            promedio = parseInt(userPuntosTecnicos + userPuntosHSE) / 2;
+        }
     },
-    mostrar: function(estudiante) {
-        let resultado = "";
-        resultado += "<div class='row'>";
-        resultado += "<div class='col m12'>";
-        resultado += "<div class='card teal grey darken-1'>";
-        resultado += "<div class='card-content white-text'>";
-        resultado += "<p><strong>Nombre:</strong> " + estudiante.nombre + "</p>";
-        resultado += "<p><strong>Puntos Técnicos:</strong> " + estudiante.puntosTecnicos + "</p>";
-        resultado += "<p><strong>Puntos HSE:</strong> " + estudiante.puntosHSE + "</p>";
-        resultado += "<p><strong>Estado:</strong> " + estudiante.estado + "</p>";
-        resultado += "</div>";
-        resultado += "</div>";
-        resultado += "</div>";
-        resultado += "</div>";
+    mostrar: function (estudiante) {
+        let resultado = ``;
+        if (estudiante != undefined) {
+            if (estudiante.nombre != null || estudiante.puntosTecnicos != null || estudiantes.puntosHSE != null) {
+                resultado += `<div class='row'>`;
+                resultado += `<div class='col m12'>`;
+                resultado += `<div class='card teal grey darken-1'>`;
+                resultado += `<div class='card-content white-text'>`;
+                resultado += `<p><strong>Nombre:</strong> ${ estudiante.nombre} </p>`;
+                resultado += `<p><strong>Puntos Técnicos:</strong> ${ estudiante.puntosTecnicos} </p>`;
+                resultado += `<p><strong>Puntos HSE:</strong> ${ estudiante.puntosHSE} </p>`;
+                resultado += `<p><strong>Estado:</strong> ${ estudiante.estado} </p>`;
+                resultado += `</div>`;
+                resultado += `</div>`;
+                resultado += `</div>`;
+                resultado += `</div>`;
+            }
+        }
         return resultado;
     },
-    mostrarLista: function(estudiantes) {
-        let resultado = "";
+    mostrarLista: function (estudiantes) {
+        let resultado = ``;
         resultado += estudiantes.map(app.mostrar);
         return resultado;
     },
-    actualizar: function(estudiantes) {
+    actualizar: function (estudiantes) {
         let lista = app.empleables(estudiantes);
         return lista;
     },
